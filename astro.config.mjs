30d2eba3318c33c2f5ffe939587a6b8aaed8345d
jhon-alex <japaricio1002@yahoo.com>
2025-04-25 16:50:22 -0500

    Configurar adaptador Vercel compatible con Astro 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/server';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+});
+
