4f540f593becc77b79f61110b6b8d0d9bee37b91
jhon-alex <japaricio1002@yahoo.com>
2025-04-25 18:58:56 -0500

    respaldo antes de actualizar a Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import staticAdapter from '@astrojs/static';
 
 export default defineConfig({
   output: 'static',
-  adapter: staticAdapter(),
 });
+
+
 
 
 
