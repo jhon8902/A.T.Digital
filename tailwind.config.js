1e70996f83c010b5a784bdeafe2d3462567cb9a7
jhon-alex <japaricio1002@yahoo.com>
2025-04-26 13:22:47 -0500

    Configurar TailwindCSS y ajustes de estilos globales

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./src/pages/**/*.astro",        // Para tus páginas en Astro
+      "./src/components/**/*.{astro,html,js}", // Para tus componentes
+      "./src/styles/**/*.css"          // Para tus archivos CSS globales
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
