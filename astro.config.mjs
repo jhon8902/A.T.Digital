beb79600eef6c8300cb4b1b6459f0fc51c596556
jhon-alex <japaricio1002@yahoo.com>
2025-09-15 15:01:42 -0500

    ultimas actualizaciones para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'server', // o 'hybrid' si usas endpoints API
-  adapter: netlify(),
+  output: 'static',
 });
 
 
